8708f263e38676395d4488a0b8146332a506f734
Marcus Povey <marcus@marcus-povey.co.uk>
2016-10-06 11:41:37 +0100

    Removing deprecated values

--- views/default/plugins/recaptcha/settings.php
+++ views/default/plugins/recaptcha/settings.php
@@ -3,8 +3,8 @@
 	$privatekey = $vars['entity']->privatekey;
 ?>
 <p><?php echo elgg_echo('recaptcha:publickey'); ?>:
-	<?php echo elgg_view('input/text', array('internalname' => 'params[pubkey]', 'value' => $pubkey)); ?>
+	<?php echo elgg_view('input/text', array('name' => 'params[pubkey]', 'value' => $pubkey)); ?>
 </p>
 <p><?php echo elgg_echo('recaptcha:privatekey'); ?>:
-	<?php echo elgg_view('input/text', array('internalname' => 'params[privatekey]', 'value' => $privatekey)); ?>
+	<?php echo elgg_view('input/text', array('name' => 'params[privatekey]', 'value' => $privatekey)); ?>
 </p>
